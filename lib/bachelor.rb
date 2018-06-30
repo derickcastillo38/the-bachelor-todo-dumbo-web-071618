f78366ef3d75c214a0ae8f94438debd6138874ab
derickcastillo38 <9derick@gmail.com>
2018-06-30 14:09:56 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_hash|
+    contestant_hash do |contestants|
+      if contestants["hometown"] == hometown
+        contestants["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
