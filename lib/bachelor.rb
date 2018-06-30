a23d461a131f676aa830c9d965e7203e4c97e347
derickcastillo38 <9derick@gmail.com>
2018-06-30 14:24:59 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,7 @@ def get_average_age_for_season(data, season)
   contestants = 0
 
   data[season].each do |contestant_hash|
-    contestant += 1
+    contestants += 1
     age += contestant_hash["age"].to_i
   end
 
